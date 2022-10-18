00b14607af4786be582721d3d75c1c63404ba352
Sticky Keys <iamstickykeys@gmail.com>
2022-10-17 17:22:28 -0700

    added a while loop to keep the game going - also added a score counter

--- CupGame.py
+++ CupGame.py
@@ -2,7 +2,7 @@
 The Cup Game
 Sticky Keys
 October 2022
-Version: 1.0.0
+Version: 1.1.0
 '''
 
 #import randon to generate a randon nubmer
@@ -11,12 +11,20 @@ import random
 print("What Cup Do You Think The Ball Is Under?")
 print("1, 2, or 3?")
 
-choice = int(input("Choice: "))
+score = 0
 
-#generate a random number from 1 to 3 
-randomCup = random.randint(1,3)
+while True:
+    
+    #generate a random number from 1 to 3 
+    randomCup = random.randint(1,3)
+    
+    choice = int(input("Choice: "))
 
-if choice == randomCup:
-    print("You Got It!")
-else:
-    print("Sorry Try Again!")
+    if choice == randomCup:
+        print("You Got It!")
+        score+=1
+    else:
+        print("Sorry Try Again!")
+        break
+    
+print("Your Final Score Is: "+str(score))
