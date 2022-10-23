ec756d96d459e98aadceee5a90f29d48b891b3f1
Sticky Keys <iamstickykeys@gmail.com>
2022-10-22 23:13:08 -0700

    made a lot of imporvments to the game, first: added a main menu feature, now you can select to play the game, learn how to play the game or quit the game. Second, moved a lot of the functionality of the game into seperate functions and call them seperately. Third added more logic to the game loop so the output looks nice and understandable to the player.

--- CupGame.py
+++ CupGame.py
@@ -2,35 +2,75 @@
 The Cup Game
 Sticky Keys
 October 2022
-Version: 1.2.0
+Version: 1.3.0
 '''
 
 #import random to generate a randon nubmer
 import random
 
-print("What Cup Do You Think The Ball Is Under?")
-print("1, 2, or 3?")
+#function that will return the intructions of the game
+def showInstructions():
+    instructions = "\nA Ball Is Put Under A Random Cup.\nThe Cups Are Shuffled.\nFind The Ball.\n"
+    print(instructions)
 
-#all the following print statementts print cups to the console
-print((("-"+("-"*3)+"-")+(" "*3))*3)
-print(("|"+((" "*3)+"|")+(" "*3))*3)
-print((("| "+ "$" + " |")+(" "*3))*3)
-print((("|"+(" "*3)+"|")+(" "*3))*3)
+#function that is used inside of launchGame() to draw cups on the screen
+def drawCups():
+    #all the follwing print statementts print the cups to the console
+    print((("-"+("-"*3)+"-")+(" "*3))*3)
+    print(("|"+((" "*3)+"|")+(" "*3))*3)
+    print((("| "+ "$" + " |")+(" "*3))*3)
+    print((("|"+(" "*3)+"|")+(" "*3))*3)
 
-score = 0
-
-while True:
+#function that will kick of the game with a specifc number of cups
+def launchGame():
+    print("Welcome, Lets Begin.\n")
     
-    #generate a random number from 1 to 3 
-    randomCup = random.randint(1,3)
+    #call drawCups() to draw a specific number of cups on the console 
+    drawCups()
     
-    choice = int(input("Choice: "))
+    #gameState keeps track of the current game state
+    # 1 = not game over
+    # 0 = game over
+    gameState = 1
+    
+    #keep track of the score
+    score = 0
+    
+    while gameState == 1:
+    
+        #print out the score
+        print("\nScore: "+str(score))
+        #generate a random cup
+        randCup = random.randint(1, 3)
+        print(randCup)
+        print("Enter \'0\' To Quit The Game.")
+        print("Shuffling The Cups...")
+        choice = int(input("Choose A Cup: "))
+        if choice == randCup:
+            print("\nCorrect!\n")
+            score+=1
+        elif choice == 0:
+            print("\nEnding Game...\n")
+            gameState == 0
+            break
+        else:
+            print("\nIncorrect!\n")
 
-    if choice == randomCup:
-        print("You Got It!")
-        score+=1
-    else:
-        print("Sorry Try Again!")
+
+#main entry point for the program 
+print("Welcome To The Cup Game!\n 1. Play\n 2. Instructions\n 3. Quit\n")
+
+choice = int(input("Choice: "))
+
+while choice != 3:
+
+    if choice == 1:
+        launchGame()
+    elif choice == 2:
+        showInstructions()
+    elif choice == 3:
         break
     
-print("Your Final Score Is: "+str(score))
+    choice = int(input("\nChoice: "))
+    
+print("Thanks For Playing. Goodbye.")
